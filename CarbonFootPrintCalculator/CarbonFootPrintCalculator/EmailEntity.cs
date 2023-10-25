0c05184df6bfdcab76820f318edfc5fcac666f9a
Prerna Mehta <65059373+Prerna6501@users.noreply.github.com>
2023-10-26 00:49:27 +0530

    Carbon foot print for email

---
+++ CarbonFootPrintCalculator/CarbonFootPrintCalculator/EmailEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbonFootPrintCalculator
+{
+    public class EmailEntity
+    {
+        public string? Email { get; set; }
+        public int NumberOfInboxEmails { get; set; }
+        public int NumberOfSentEmails { get; set; }
+        public int NumberOfSpamEmails { get; set; }
+    }
+}
