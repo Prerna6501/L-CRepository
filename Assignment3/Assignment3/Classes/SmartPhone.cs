67a6831a36e612836134362f3249af2faf20cf7b
Prerna Mehta <65059373+Prerna6501@users.noreply.github.com>
2023-12-26 21:53:36 +0530

    Add files via upload

---
+++ Assignment3/Assignment3/Classes/SmartPhone.cs
@@ -0,0 +1,17 @@
+﻿using Assignment3.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Classes
+{
+    public class SmartPhone : IDevice
+    {
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Smartphone: Model X, RAM: 8GB, Storage: 128GB");
+        }
+    }
+}
---
+++ Assignment3/Assignment3/Classes/SmartPhoneFactory.cs
@@ -0,0 +1,17 @@
+﻿using Assignment3.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Classes
+{
+    public class SmartPhoneFactory : IDeviceFactory
+    {
+        public IDevice CreateDevice()
+        {
+            return new SmartPhone();
+        }
+    }
+}
