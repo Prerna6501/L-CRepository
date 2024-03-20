26497785c6221f95a43a59ddfceb2784cfa4f9e3
Prerna Mehta <65059373+Prerna6501@users.noreply.github.com>
2024-03-20 13:40:43 +0530

    Add files via upload
    Getter Setter

---
+++ PersonClassWithPrivateSetters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetterSetterAssignment
+{
+    public class PersonClassWithPrivateSetters
+    {
+        public string Name { get;  }
+        public int Age { get; }
+        public PersonClassWithPrivateSetters(string name , int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
